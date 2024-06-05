70a7159a7c2a3cd0245d3bc1f9fdd8b726722a76
Isaacxavierlee <reivax.caasi@gmail.com>
2024-06-05 14:34:08 +0800

    bank statements

--- components/ui/TotalBalanceBox.tsx
+++ components/ui/TotalBalanceBox.tsx
@@ -8,15 +8,24 @@ const TotalBalancebox = ({
   return (
     <section className='total-balance'>
       <div className='total-balance-chart'>
-        Total Balance Box
+        {/* Uncomment and insert DonutChart component here */}
+        {/* <DonutChart data={accounts} /> */}
       </div>
+
       <div className='flex flex-col gap-6'>
         <h2 className='header-2'>
           Bank Accounts: {totalBanks}
         </h2>
-        <p className='header-2'>
-          Total Current Balance: ${totalCurrentBalance}
-        </p>
+
+        <div className='flex flex-col gap-2'>
+          <p className='total-balance-label'>
+            Total Current Balance: ${totalCurrentBalance.toFixed(2)}
+          </p>
+
+          <p className='total-balance-amount flex-center gap-2'>
+            {totalCurrentBalance.toFixed(2)}
+          </p>
+        </div>
       </div>
     </section>
   );
