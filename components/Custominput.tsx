a842cca8b95c73ab5f09d01ee456f9a4945be48c
Isaacxavierlee <reivax.caasi@gmail.com>
2024-06-09 18:28:44 +0800

    can update a few now is console time

--- components/Custominput.tsx
+++ components/Custominput.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FormControl, FormField, FormLabel, FormMessage } from './ui/form';
-import { Input } from './ui/input';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { Control, FieldPath } from 'react-hook-form';
 import { z } from 'zod';
 import { authformSchema } from '@/lib/utils';
@@ -17,23 +17,18 @@ const CustomInput: React.FC<CustomInputProps> = ({ control, name, label, placeho
     <FormField
       control={control}
       name={name}
-      render={({ field }) => (
-        <div className="form-item">
-          <FormLabel className="form-label">
-            {label}
-          </FormLabel>
-          <div className="flex w-full flex-col">
-            <FormControl>
-              <Input 
-                placeholder={placeholder}
-                className="input-class"
-                type={name === 'password' ? 'password' : 'text'}
-                {...field}
-              />
-            </FormControl>
-            <FormMessage className="form-message mt-2" />
-          </div>
-        </div>
+      render={({ field, fieldState: { error } }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input 
+              placeholder={placeholder}
+              {...field}
+              type={name === 'password' ? 'password' : 'text'}
+            />
+          </FormControl>
+          {error && <FormMessage>{error.message}</FormMessage>}
+        </FormItem>
       )}
     />
   );
