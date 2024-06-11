4dea1f09524bd612c198d495efb6097aab3aa745
Isaacxavierlee <reivax.caasi@gmail.com>
2024-06-11 12:28:01 +0800

    in&outcrossover

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import Sidebar from "@/components/Sidebar";
 import MobileNav from "@/components/ui/MobileNav";
-import Image from 'next/image';
+import Sidebar from "@/components/Sidebar";
 import { getLoggedInUser } from '@/lib/actions/user.actions';
+
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   const loggedIn = await getLoggedInUser();
+
+  if(!loggedIn) redirect('/sign-in')
 
   return (
     <main className="flex h-screen w-full font-inter">
